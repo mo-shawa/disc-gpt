cc0a468395cd0125f653aadd905f239233a64474
moshawa <moshawa0@gmail.com>
2023-03-18 22:45:25 -0400

    minimal working bot

--- src/api/GPT.ts
+++ src/api/GPT.ts
@@ -1,26 +1,16 @@
 import { Configuration, OpenAIApi } from "openai"
 import { GPTKey } from "../private/token.json"
 const configuration = new Configuration({
-	// organization: "org-D4eQrRtITyR5KHRrRSqDUMlq",
 	apiKey: GPTKey,
 })
 const openai = new OpenAIApi(configuration)
 
-async function getResponse() {
-	const res = await openai.listEngines()
-	console.log(res.data.data)
-}
-// const response = await openai.listEngines()
-
-// getResponse()
-
-export async function promptEngine(prompt: string) {
+export async function promptEngine(prompt: any) {
 	const res = await openai.createCompletion({
 		model: "text-davinci-003",
 		prompt: prompt,
 		max_tokens: 400,
 	})
-	console.log(res.data.choices[0].text)
-}
 
-promptEngine("write a poem about a hairy boy named aboudat")
+	return res.data.choices[0].text
+}
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,56 @@
-import { Client, ClientOptions } from 'discord.js';
-import { token } from "./private/token.json"; // Todo - create bot in discord dev portal and add token
+import { Client, GatewayIntentBits } from "discord.js"
+import { token } from "./private/token.json" // Todo - create bot in discord dev portal and add token
+import { promptEngine } from "./api/GPT"
+
+const { Guilds, GuildMessages } = GatewayIntentBits
 
 const client = new Client({
-  intents: []
-}); // Todo - specify options depending on scope of project
+	intents: [Guilds, GuildMessages],
+}) // Todo - specify options depending on scope of project
 
-client.login(token);
+client.login(token)
 
-client.once('ready', () => {
-  console.log(`Bot is running - ${client.user?.tag}`);
+client.once("ready", () => {
+	console.log(`Bot is running - ${client.user?.tag}`)
+})
+
+client.on("interactionCreate", async (interaction) => {
+	console.log("entered interaction")
+	if (!interaction.isCommand()) return
+
+	if (interaction.commandName === "test") {
+		const prompt = interaction.options.get("prompt")
+		console.log("before defer")
+		// try ephemeral: true
+		await interaction.deferReply()
+		console.log("after defer")
+
+		const res = await promptEngine(prompt!.value)
+		console.log("after res")
+
+		await interaction.editReply(res as string)
+		// try reply if edit doesnt work
+	}
 })
 
 /**
  * Here is the syntax for having the bot watch for a message to initate an interaction:
- * 
+ *
  * client.on('messageCreate', async (message) => {
  *   if (message.author.username.toLowerCase() === 'nrtz') {
  *    return await message.reply('rat');
  *  }
  *  return;
  * });
- * 
+ *
  */
 
 /**
  * Here is the syntax for having the bot watch for an 'interaction' (user entered command):
- * 
+ *
  * client.on('interactionCreate', async (interaction) => {
  *  if (!interaction.isCommand()) return;
- *  
+ *
  *  if (interaction.commandName === "commandname") {
  *    ----- command logic goes here -----
  *  }
